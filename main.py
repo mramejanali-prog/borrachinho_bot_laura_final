345395c0a10fdeceb206f6a29990586cc224c4a2
mramejanali-prog <mramejanali@gmail.com>
2025-07-29 18:59:01 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import asyncio
-from telegram.ext import ApplicationBuilder, CommandHandler
 
-TOKEN = "8140620333:AAF-yB_m0NThmf7KSYvWm4pC03HlBAZ-ZEA"
+from telegram.ext import ApplicationBuilder, CommandHandler
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update, context):
-    await update.message.reply_text("Olá, eu sou o Borrachinho_Bot_Laura! 💋")
+    await update.message.reply_text("Olá! Borrachinho Bot está ativo.")
 
-async def main():
-    application = ApplicationBuilder().token(TOKEN).build()
-    application.add_handler(CommandHandler("start", start))
-    await application.run_polling()
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
